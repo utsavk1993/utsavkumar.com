a22ff1c08f80fee17d515c812e29ea59e31b2b54
Utsav Kumar <utsavkumar.ca@gmail.com>
2025-01-03 13:27:02 -0800

    Changed some styles on about page

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,7 +7,8 @@ export default function Portfolio({ innerRef }) {
   return (
     <Box id={"portfolio"} ref={innerRef}>
       <Grid container display={"flex"} justifyContent={"center"}>
-        {info.portfolio.map((project, index) => (
+        <h1>Coming soon!</h1>
+        {/* {info.portfolio.map((project, index) => (
           <Grid item xs={12} md={6} key={index}>
             <PortfolioBlock
               image={project.image}
@@ -16,7 +17,7 @@ export default function Portfolio({ innerRef }) {
               title={project.title}
             />
           </Grid>
-        ))}
+        ))} */}
       </Grid>
     </Box>
   );
