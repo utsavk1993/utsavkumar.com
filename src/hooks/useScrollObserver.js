f267fe6706ecc601d158e437bad82676ac013ee7
Utsav Kumar <utsavkumar.ca@gmail.com>
2025-01-03 12:54:44 -0800

    Formatted the code and added some content to the about page

--- src/hooks/useScrollObserver.js
+++ src/hooks/useScrollObserver.js
@@ -6,17 +6,17 @@ import { useInView } from "react-intersection-observer";
  * ref can be assigned to any JSX element as prop, and
  * when the element reaches 50% of the viewport, the passed
  * in function will be called with the element id as parameter.
- * @param {function} action 
+ * @param {function} action
  * @returns {funciton} ref
  */
 export default function useScrollObserver(action) {
-    const {ref, inView, entry} = useInView({
-        rootMargin: '-50% 0% -50% 0%'
-    })
-    useEffect(() => {
-        if (entry && inView) {
-            action(entry.target.id);
-        }
-    }, [entry, action, inView])
-    return ref
+  const { ref, inView, entry } = useInView({
+    rootMargin: "-50% 0% -50% 0%",
+  });
+  useEffect(() => {
+    if (entry && inView) {
+      action(entry.target.id);
+    }
+  }, [entry, action, inView]);
+  return ref;
 }
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,14 +1,14 @@
-import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import self from "../img/self.png";
+import mock1 from "../img/mock1.png";
+import mock2 from "../img/mock2.png";
+import mock3 from "../img/mock3.png";
+import mock4 from "../img/mock4.png";
+import mock5 from "../img/mock5.png";
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export const colors = ["rgb(42, 197, 184)", "rgb(54, 133, 193)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -18,7 +18,7 @@ the site.
 /* 
 This variable will change the layout of the website from multipage to single, scrollable page
 */
-export let singlePage = false;
+export const singlePage = false;
 
 /*
 So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
@@ -27,100 +27,132 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "Utsav",
-    lastName: "Kumar",
-    initials: "uk", // the example uses first and last, but feel free to use three or more if you like.
-    position: "Senior Software Engineer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
-    baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
-        {
-            emoji: '☕',
-            text: 'fueled by coffee'
-        },
-        {
-            emoji: '🌎',
-            text: 'based in canada'
-        },
-        {
-            emoji: "💼",
-            text: "Senior Software Engineer at Highspot"
-        },
-        {
-            emoji: "📧",
-            text: "utsavkumar.ca@gmail.com"
-        }
+  firstName: "Utsav",
+  lastName: "Kumar",
+  initials: "uk", // the example uses first and last, but feel free to use three or more if you like.
+  position: "Senior Front-end Developer",
+  selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+  baseColor: colors[0],
+  miniBio: [
+    // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    {
+      emoji: "🔋",
+      text: "fueled by energy drink",
+    },
+    {
+      emoji: "⛰️",
+      text: "based in vancouver, canada",
+    },
+    {
+      emoji: "💼",
+      text: "Senior Software Engineer at Highspot",
+    },
+    {
+      emoji: "📧",
+      text: "utsavkumar.ca@gmail.com",
+    },
+  ],
+  socials: [
+    {
+      link: "https://github.com/utsavk1993",
+      icon: "fa fa-github",
+      label: "github",
+    },
+    {
+      link: "https://www.linkedin.com/in/utsavk1993/",
+      icon: "fa fa-linkedin",
+      label: "linkedin",
+    },
+    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+    // Just change the links so that they lead to your social profiles.
+  ],
+  bio: "Hi! I’m Utsav, a software engineer at Highspot with over 9 years of experience building elegant front-end solutions using React and TypeScript. I studied Computer Science at the University of Windsor. When I’m not coding, you can find me exploring the latest in AI or crafting modern, user-friendly web apps. Let’s build something amazing together!",
+  skills: {
+    proficientWith: [
+      "javascript",
+      "react",
+      "typescript",
+      "html5",
+      "css3",
+      "jest",
+      "cypress",
     ],
-    socials: [
-        {
-            link: "https://github.com/utsavk1993",
-            icon: "fa fa-github",
-            label: 'github'
-        },
-        {
-            link: "https://www.linkedin.com/in/utsavk1993/",
-            icon: "fa fa-linkedin",
-            label: 'linkedin'
-        },
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
-
+    exposedTo: [
+      "nodejs",
+      "figma",
+      "canva",
+      "postgresql",
+      "mongodb",
+      "docker",
+      "supabase",
     ],
-    bio: "Hello! I'm Utsav. I'm a software engineer for Highspot. I studied CompSci at University of Windsor, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
-    skills:
-        {
-            proficientWith: ['javascript', 'react', 'typescript', 'node.js', 'html5', 'css3', 'jest', 'cypress'],
-            exposedTo: ['nodejs', 'figma', 'canva', 'postgresql', 'mongodb', 'docker', 'supabase']
-        }
-    ,
-    hobbies: [
-        {
-            label: 'travel',
-            emoji: '🌎'
-        },
-        {
-            label: 'music',
-            emoji: '🎵'
-        },
-        {
-            label: 'movies',
-            emoji: '🎥'
-        },
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
-    ],
-    portfolio: [],
-    // portfolio: [ // This is where your portfolio projects will be detailed
-    //     {
-    //         title: "Project 1",
-    //         live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-    //         source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-    //         image: mock1
-    //     },
-    //     {
-    //         title: "Project 2",
-    //         live: "https://paytonpierce.dev",
-    //         source: "https://github.com/paytonjewell",
-    //         image: mock2
-    //     },
-    //     {
-    //         title: "Project 3",
-    //         live: "https://paytonpierce.dev",
-    //         source: "https://github.com/paytonjewell",
-    //         image: mock3
-    //     },
-    //     {
-    //         title: "Project 4",
-    //         live: "https://paytonpierce.dev",
-    //         source: "https://github.com/paytonjewell",
-    //         image: mock4
-    //     },
-    //     {
-    //         title: "Project 5",
-    //         live: "https://paytonpierce.dev",
-    //         source: "https://github.com/paytonjewell",
-    //         image: mock5
-    //     }
-    // ]
-}
+  },
+  experience: [
+    {
+      label: "Senior Software Engineer at Highspot",
+      duration: "Jun 2023 - Present",
+    },
+    {
+      label: "Software Engineer at Splunk",
+      duration: "Feb 2019 - Feb 2023",
+    },
+    {
+      label: "Software Engineer at Cybage Software",
+      duration: "Jun 2015 - Aug 2017",
+    },
+    {
+      label: "Jr. Web Developer at Weblineindia",
+      duration: "Jun 2014 - Jun 2015",
+    },
+  ],
+  hobbies: [
+    {
+      label: "travel",
+      emoji: "🌎",
+    },
+    {
+      label: "music",
+      emoji: "🎵",
+    },
+    {
+      label: "movies",
+      emoji: "🎥",
+    },
+    // Same as above, change the emojis to match / relate to your hobbies or interests.
+    // You can also remove the emojis if you'd like, I just think they look cute :P
+  ],
+  portfolio: [],
+  // portfolio: [ // This is where your portfolio projects will be detailed
+  //     {
+  //         title: "Project 1",
+  //         live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+  //         source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+  //         image: mock1
+  //     },
+  //     {
+  //         title: "Project 2",
+  //         live: "https://paytonpierce.dev",
+  //         source: "https://github.com/paytonjewell",
+  //         image: mock2
+  //     },
+  //     {
+  //         title: "Project 3",
+  //         live: "https://paytonpierce.dev",
+  //         source: "https://github.com/paytonjewell",
+  //         image: mock3
+  //     },
+  //     {
+  //         title: "Project 4",
+  //         live: "https://paytonpierce.dev",
+  //         source: "https://github.com/paytonjewell",
+  //         image: mock4
+  //     },
+  //     {
+  //         title: "Project 5",
+  //         live: "https://paytonpierce.dev",
+  //         source: "https://github.com/paytonjewell",
+  //         image: mock5
+  //     }
+  // ]
+};
