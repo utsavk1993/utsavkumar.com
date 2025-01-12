7e3988f15e16f38d4fb7296f0243ff404bee35e4
Utsav Kumar <utsavkumar.ca@gmail.com>
2025-01-11 18:37:35 -0800

    Added vercel analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.module.scss';
 import BaseLayout from './components/BaseLayout';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <BaseLayout />
       </BrowserRouter>
+      <Analytics />
     </div>
   );
 }
